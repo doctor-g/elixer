90fa058d8bbbe70632f70c45d0f99fe741db876c
Paul Gestwicki <pvgestwicki@bsu.edu>
2019-10-10 19:26:36 -0400

    Add name of the wind support

--- src/components/elixer-app.js
+++ src/components/elixer-app.js
@@ -1,11 +1,15 @@
 import { LitElement, html, css } from 'lit-element';
+import '@polymer/paper-checkbox/paper-checkbox.js';
 import './score-table.js';
 
 class ElixerApp extends LitElement {
   static get properties() {
     return {
-      conf: {
+      __conf: {
         type: Array
+      },
+      __nameOfTheWind: {
+        type: Object
       }
     };
   }
@@ -16,19 +20,35 @@ class ElixerApp extends LitElement {
         :host {
           display: block;
         }
+        .booktitle {
+          font-style: italic;
+        }
       `
     ]
   }
 
   render() {
     return html`
-      <score-table .rowConfig="${this.conf}"></score-table>  
+      <score-table .rowConfig="${this.__conf}"></score-table> 
+      <h2>Options</h2>
+      <paper-checkbox @change=${this.__onNameOfTheWindCheck}>
+        Use <span class="booktitle">Name of the Wind</span> Expansion
+      </paper-checkbox>
     `;
+  }
+
+  __onNameOfTheWindCheck(event) {
+    if (event.target.checked) {
+      this.__conf = [...this.__conf, this.__nameOfTheWind];
+    } else {
+      this.__conf = this.__conf.splice(0, this.__conf.length-1);
+    }
+    console.log(event.target.checked);
   }
 
   constructor() {
     super();
-    this.conf = [
+    this.__conf = [
       'Triumph',
       'Tragedy',
       'Hero Cards',
@@ -36,6 +56,7 @@ class ElixerApp extends LitElement {
       'Experience',
       'Story Icons'
     ];
+    this.__nameOfTheWind = 'Name Runes';
   }
 
 }
