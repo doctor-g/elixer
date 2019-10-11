dba29e3110c649f184c234d4e8574375bf779663
Paul Gestwicki <pvgestwicki@bsu.edu>
2019-10-10 20:04:16 -0400

    Broken

--- src/components/elixer-app.js
+++ src/components/elixer-app.js
@@ -3,74 +3,7 @@ import '@polymer/paper-checkbox/paper-checkbox.js';
 import './score-table.js';
 
 class ElixerApp extends LitElement {
-  static get properties() {
-    return {
-      __conf: {
-        type: Array
-      },
-      __nameOfTheWind: {
-        type: Object
-      }
-    };
-  }
-
-  static get styles() {
-    return [
-      css`
-        :host {
-          display: block;
-        }
-        .booktitle {
-          font-style: italic;
-        }
-      `
-    ]
-  }
-
-  render() {
-    return html`
-      <score-table .rowConfig="${this.__conf}"></score-table> 
-      <h2>Options</h2>
-      <paper-checkbox @change=${this.__onNameOfTheWindCheck}>
-        Use <span class="booktitle">Name of the Wind</span> Expansion
-      </paper-checkbox>
-    `;
-  }
-
-  __onNameOfTheWindCheck(event) {
-    if (event.target.checked) {
-      this.__conf = [...this.__conf, this.__nameOfTheWind];
-    } else {
-      this.__conf = this.__conf.splice(0, this.__conf.length-1);
-    }
-  }
-
-  constructor() {
-    super();
-    this.__conf = [
-      {
-        icon: 'triumph.png'
-      },
-      {
-        icon: 'tragedy.png'
-      },
-      {
-        icon: 'hero.png'
-      },
-      {
-        icon: 'antihero.png'
-      },
-      {
-        icon: 'experience.png'
-      },
-      {
-        icon: 'nature.png'
-      }
-    ];
-    this.__nameOfTheWind = {
-      icon: 'name.png'
-    };
-  }
+  
 
 }
 
